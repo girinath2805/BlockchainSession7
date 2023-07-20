ed641eb2ac9267606da9d328166ca8df4b4d4c5b
girinath2805 <133040026+girinath2805@users.noreply.github.com>
2023-07-20 09:19:41 +0530

    script_3.js

--- script_3.js
+++ script_3.js
@@ -1,28 +1,23 @@
-import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/5.7.2/ethers.esm.min.js";
+import {ethers} from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.6.4/ethers.min.js";
 
-const provider = new ethers.providers.JsonRpcProvider('YourAPIkeylink');
+const address = "0x13116fbddB92A853C86Bc9c0E6811e72629Ad570";
+const privateKey = 'd46347fe69bc0183ff95660a8ed8cb0d01401c0cb5501834023f1961af1164b2'
 
-
-const privateKey = '';
-const address = '';
-
-const wallet = new ethers.Wallet(privateKey,provider);
-const transfer = {
-    to : address,
-    value: ethers.utils.parseEther("0.01")
-}
+const provider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3/68d5e602045040fcbb3ecc41d59ee720');
 
 const main = async() => {
-    const tx = await wallet.sendTransaction(transfer);
+    const wallet = new ethers.Wallet(privateKey,provider);
+    const tx = await wallet.sendTransaction({
+        to: address,
+        value: ethers.parseEther('0.01')
+    })
 
     const receipt = await tx.wait();
-
-    console.log(`Transaction hash : ${receipt.transactionHash}`);
+    console.log(`Transaction hash : ${tx.hash}`);
     console.log(`Block number : ${receipt.blockNumber}`);
     console.log(`Gas fee : ${receipt.gasUsed.toString()}`);
     console.log(`Status : ${receipt.status}`);
     console.log('Logs:', receipt.logs);
 
 }
-
 main();
