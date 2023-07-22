c22412dc7e40ae14a14abab56ceaf16ec7972d6f
girinath2805 <133040026+girinath2805@users.noreply.github.com>
2023-07-22 09:40:21 +0530

    script_2.js

--- script_2.js
+++ script_2.js
@@ -1,24 +1,26 @@
 import {ethers} from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.6.4/ethers.min.js";
 
-const address = "0x359Ab2b7163Af148FF2e77f2FAEC255DC5CEa392";
-const ABI = ["function getValue() public view returns(uint256)",
-             "function AddNumber(uint256 _firstNumber, uint256 _secondNumber) public"          
-    ];
-
-const privatekey = "6cf75a06ba2e952ab52cc40444925994f456a1af0163e1cb19e44987b5dfff74";
 const provider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3/68d5e602045040fcbb3ecc41d59ee720');
+const ABI = ["function setValue(uint256 _number) public",
+             "function getValue() public view returns(uint256)"];
+
+const privatekey = 'd46347fe69bc0183ff95660a8ed8cb0d01401c0cb5501834023f1961af1164b2';
+const address = "0x478fe1cee0101d215f78cc24578136544300c523";
+const wallet = new ethers.Wallet(privatekey,provider);
 
 const main = async() => {
-    const wallet = new ethers.Wallet(privatekey,provider);
-    const contract = new ethers.Contract(address,ABI,wallet);
+   const writeContract = new ethers.Contract(address,ABI,wallet);
 
-    const tx = await contract.AddNumber(25,23);
-    const receipt = await tx.wait();
-    console.log('Transaction hash : ',tx.hash);
+   const tx = await writeContract.setValue(100);
+   console.log(`Trasnsaction Hash : ${tx.hash}`);
+   const receipt = await tx.wait();
+   console.log("Transaction Mined!");
+   console.log(`Gas Used : ${receipt.gasUsed}`);
+   console.log(`Transaction Status : ${receipt.status}`);
+   console.log(`Block : ${receipt.blockNumber}`);
 
-    const Read = await contract.getValue();
-
-    console.log(Read.toString());
-
+   const value = await writeContract.getValue();
+   console.log(`The value that has been set --> ${value}`);
 }
-main(); 
+
+main();
